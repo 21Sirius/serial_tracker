94a271b99d5a32634f05e672bbac660858ce3122
21Sirius <21siriusss@gmail.com>
2024-02-07 17:54:50 +0700

    first simple commit

---
+++ project_root1/main/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'главная страиница трекера сериалов',
+        'list': ['first', 'second'],
+        'dict': {'first':1},
+        'is_auth':  True
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse('About page')
