6725a6aae0495e01da8b1d25db95b49ebd06e790
21Sirius <21siriusss@gmail.com>
2024-02-08 23:22:53 +0700

    создано приложение serials и проработан шаблоны seiral i catalog

--- project_root1/project_root/urls.py
+++ project_root1/project_root/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('serials.urls', namespace='catalog'))
+
 ]
