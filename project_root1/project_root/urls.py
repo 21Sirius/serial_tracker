101083d7f119c4bdbd59bed06dd61f034248cb19
21Sirius <21siriusss@gmail.com>
2024-02-18 19:17:54 +0700

    Работа с добавлением изображений + установка и  настройка debug toolbar

--- project_root1/project_root/urls.py
+++ project_root1/project_root/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 
-
-
+from project_root import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('serials.urls', namespace='catalog'))
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
