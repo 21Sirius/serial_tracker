b5c38457ceb6b632d13ffb42c5385ede86aaf08c
21Sirius <21siriusss@gmail.com>
2024-02-19 14:56:46 +0700

    Добавление слагов для урлс

--- project_root1/serials/urls.py
+++ project_root1/serials/urls.py
@@ -5,7 +5,7 @@ from serials import views
 app_name = 'serials'
 
 urlpatterns = [
-    path('', views.catalog, name='index'),
-    path('serial/', views.serial, name='serial')
+    path('<slug:genre_slug>', views.catalog, name='index'),
+    path('serial/<slug:serial_slug>/', views.serial, name='serial'),
 
 ]
--- project_root1/serials/views.py
+++ project_root1/serials/views.py
@@ -4,8 +4,13 @@ from serials.models import Serial
 
 
 # Create your views here.
-def catalog(request):
-    serials = Serial.objects.all()
+def catalog(request, genre_slug):
+
+    if genre_slug == 'vse-serialy':
+        serials = Serial.objects.all()
+    else:
+        serials = Serial.objects.filter(genre__slug=genre_slug)
+
     context = {
         'title': 'Список сериалов',
         'serials': serials
@@ -13,5 +18,12 @@ def catalog(request):
     return render(request, 'serials/catalog.html', context)
 
 
-def serial(request):
-    return render(request, 'serials/serial.html')
+def serial(request, serial_slug=False,):
+    serial = Serial.objects.get(slug=serial_slug)
+
+    context = {
+        'serial': serial
+    }
+
+    return render(request, 'serials/serial.html', context)
+
